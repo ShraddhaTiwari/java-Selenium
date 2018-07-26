def6b133a60e7838e8009cd3dab13efb7333e91c
Admin <Admin@Admin-PC>
2018-07-26 22:50:00 +0530

    My First Program

---
+++ MyProject/src/myPackage/FirstPrg.java
@@ -0,0 +1,12 @@
+package myPackage;
+
+public class FirstPrg {
+
+	public static void main(String[] args) 
+	{
+		System.out.println("Welcome in Github");
+		System.out.println("This is my first program");
+
+	}
+
+}
